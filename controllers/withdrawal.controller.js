161fb80effcec2eb0bc643a908b7feedc083860d
Mbata Precious <mbataprecious9@gmail.com>
2023-12-23 07:11:37 +0100

    bronze activation

--- controllers/withdrawal.controller.js
+++ controllers/withdrawal.controller.js
@@ -13,7 +13,7 @@ export const makeWithdrawal = async (req, res) => {
   const currentBalance = req.profile.accountBalance;
   const { amount, currency } = req.body;
   console.log(req.body);
-  return response(res, 401, "Upgrade your s9 connection to bronze", null);
+  return response(res, 401, "ERR: Activate your bronze level", null);
   /**
  * 
  
